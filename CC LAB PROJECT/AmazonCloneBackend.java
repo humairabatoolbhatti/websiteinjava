55f7e9b6bd8450b80a1e3621655b2d55d9c5cc55
humairabatoolbhatti <batoolhumaira37@gmail.com>
2024-12-23 20:00:32 +0500

    Add files via upload

---
+++ CC LAB PROJECT/AmazonCloneBackend.java
@@ -0,0 +1,10 @@
+package com.amazonclone.backend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCloneBackend {
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonCloneBackend.class, args);
+    }
+}
